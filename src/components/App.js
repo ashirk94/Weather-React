255cea36f9b317df341c3f9a46c5ba76e3b78ed3
Alan Shirk <ashirk94@gmail.com>
2022-01-28 23:40:53 -0800

    setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Weather <code>le code</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
