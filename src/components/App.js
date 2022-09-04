c101f3acb00ebd130340e2fc9959388c1ee16b98
Alan Shirk <ashirk94@gmail.com>
2022-09-04 10:34:23 -0700

    Fixed API promise logic and state variables

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,94 @@
-import React, { Component } from "react";
-import "./App.css";
-import ZipForm from "./ZipForm";
-import WeatherList from "./WeatherList";
-import CurrentDay from "./CurrentDay";
-import { get } from "axios";
+import React, { Component } from 'react'
+import './App.css'
+import ZipForm from './ZipForm'
+import WeatherList from './WeatherList'
+import CurrentDay from './CurrentDay'
+import { get } from 'axios'
 
 //main component
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      zipcode: "",
-      city: {},
-      forecast: [],
-      selectedDate: null,
-    };
-    this.url = "https://api.openweathermap.org/data/2.5/onecall?";
-    this.apikey = '&exclude=minutely,hourly,current&units=imperial&appid=' + process.env.WEATHER_API_KEY
-      
-    this.googleKey = process.env.GOOGLE_API_KEY
+	constructor() {
+		super()
+		this.state = {
+			zipcode: '',
+			city: {},
+			forecast: [],
+			selectedDate: null
+		}
+		this.url = 'https://api.openweathermap.org/data/2.5/onecall?'
+		this.apikey =
+			'&exclude=minutely,hourly,current&units=imperial&appid=' +
+			process.env.REACT_APP_WEATHER_API_KEY
 
-    // url pattern for api call
-    this.geocodeUrl =
-      "https://maps.googleapis.com/maps/api/geocode/json?region=us&address=";
+		this.googleKey = process.env.REACT_APP_GOOGLE_API_KEY
 
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.onDayClicked = this.onDayClicked.bind(this);
-  }
-  render() {
-    const { forecast, timezoneOffset, selectedDate, city } = this.state;
-    return (
-      <div id="app-container">
-        <div className="app">
-          <ZipForm onSubmit={this.onFormSubmit} />
-          <WeatherList
-            forecastDays={forecast}
-            timezoneOffset={timezoneOffset}
-            onDayClicked={this.onDayClicked}
-          />
-          {selectedDate !== null && (
-            <CurrentDay
-              forecastDay={forecast[selectedDate]}
-              city={city}
-              timezoneOffset={timezoneOffset}
-            />
-          )}
-        </div>
-      </div>
-    );
-  }
-  //made api calls async to account for retrieval time
-  async onFormSubmit(zipcode) {
-    //google maps api call
-    await get(`${this.geocodeUrl}${zipcode}&key=${this.googleKey}`)
-      .then(({ data }) => {
-        console.log(data);
-        let curCity = {
-          name: data.results[0].address_components[1].long_name,
-          lat: data.results[0].geometry.location.lat,
-          lon: data.results[0].geometry.location.lng,
-        };
-        this.setState({ city: curCity });
-      })
-      .catch((error) => {
-        alert(error);
-      });
-    //weather api call
-    await get(
-      `${this.url}lat=${this.state.city.lat}&lon=${this.state.city.lon}${this.apikey}`
-    )
-      .then(({ data }) => {
-        console.log(data);
-        data.daily.splice(7);
-        this.setState({
-          forecast: data.daily,
-          timezoneOffset: data.timezone_offset,
-        });
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
-  onDayClicked(dayIndex) {
-    this.setState({ selectedDate: dayIndex });
-  }
+		// url pattern for api call
+		this.geocodeUrl =
+			'https://maps.googleapis.com/maps/api/geocode/json?address='
+
+		this.onFormSubmit = this.onFormSubmit.bind(this)
+		this.onDayClicked = this.onDayClicked.bind(this)
+	}
+	render() {
+		const { forecast, timezoneOffset, selectedDate, city } = this.state
+		return (
+			<div id='app-container'>
+				<div className='app'>
+					<ZipForm onSubmit={this.onFormSubmit} />
+					<WeatherList
+						forecastDays={forecast}
+						timezoneOffset={timezoneOffset}
+						onDayClicked={this.onDayClicked}
+					/>
+					{selectedDate !== null && (
+						<CurrentDay
+							forecastDay={forecast[selectedDate]}
+							city={city}
+							timezoneOffset={timezoneOffset}
+						/>
+					)}
+				</div>
+			</div>
+		)
+	}
+	//made api calls async to account for retrieval time
+	onFormSubmit(zipcode) {
+		//google maps api call
+		get(`${this.geocodeUrl + zipcode}&key=${this.googleKey}`)
+			.then(({ data }) => {
+				console.log(data)
+				let curCity = {
+					name: data.results[0].address_components[1].long_name,
+					lat: data.results[0].geometry.location.lat,
+					lon: data.results[0].geometry.location.lng
+				}
+				this.setState({city: curCity})
+
+				//weather api call
+				get(
+					`${this.url}lat=${this.state.city.lat}&lon=${this.state.city.lon}${this.apikey}`
+				)
+					.then(({ data }) => {
+						console.log(data)
+						data.daily.splice(7)
+						this.setState({
+							forecast: data.daily,
+							timezoneOffset: data.timezone_offset
+						})
+					})
+					.catch((error) => {
+						//alert(error)
+						console.log(error)
+					})
+			})
+			.catch((error) => {
+				//alert(error)
+				console.log(error)
+			})
+	}
+	onDayClicked(dayIndex) {
+		this.setState({ selectedDate: dayIndex })
+	}
 }
 
-export default App;
+export default App
--- src/components/ZipForm.js
+++ src/components/ZipForm.js
@@ -1,49 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 //form - getting zipcode from user
 class ZipForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      zipcode: "",
-    };
-    this.inputChanged = this.inputChanged.bind(this);
-    this.submitZipCode = this.submitZipCode.bind(this);
-  }
-  render() {
-    return (
-      <div className="zip-form">
-        <form id="zipForm" onSubmit={this.submitZipCode}>
-          <div className="flex-parent">
-            <label htmlFor="zipcode">Zip</label>
-            <input
-              className="form-control"
-              type="input"
-              id="zipcode"
-              name="zipcode"
-              value={this.state.zipcode}
-              required
-              onChange={this.inputChanged}
-            />
-            <button type="submit" className="btn btn-success">
-              Get the forcast!
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-  inputChanged(event) {
-    const { value } = event.target;
-    this.setState({ zipcode: value });
-  }
-  submitZipCode(event) {
-    event.preventDefault();
-    const { zipcode } = this.state.zipcode;
-    const { onSubmit } = this.props;
-    onSubmit(zipcode);
-    this.setState({ zipcode: "" });
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			zipcode: ''
+		}
+		this.inputChanged = this.inputChanged.bind(this)
+		this.submitZipCode = this.submitZipCode.bind(this)
+	}
+	render() {
+		return (
+			<div className='zip-form'>
+				<form id='zipForm' onSubmit={this.submitZipCode}>
+					<div className='flex-parent'>
+						<label htmlFor='zipcode'>Zip</label>
+						<input
+							className='form-control'
+							type='input'
+							id='zipcode'
+							name='zipcode'
+							value={this.state.zipcode}
+							required
+							onChange={this.inputChanged}
+						/>
+						<button type='submit' className='btn btn-success'>
+							Get the forcast!
+						</button>
+					</div>
+				</form>
+			</div>
+		)
+	}
+	inputChanged(event) {
+		const { value } = event.target
+		this.setState({ zipcode: value })
+	}
+	submitZipCode(event) {
+		event.preventDefault()
+		const { onSubmit } = this.props
+		onSubmit(this.state.zipcode)
+	}
 }
 
-export default ZipForm;
+export default ZipForm
